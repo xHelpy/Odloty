13df1e5d51d18fe8521082c1fe34631a9c8a98b1
5m <5m@DESKTOP-EHD8S3P>
2025-09-16 11:15:22 +0200

    Dodaj pliki projektów.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+
+        {
+
+            InitializeComponent();
+
+            // DateTime dt = new DateTime(2024, 1, 2); 
+
+            DateTime dt = DateTime.Now;
+
+            dpDate1.Date = dt;
+
+
+
+        }
+
+
+
+        private void onDateSelected(object sender, EventArgs e)
+
+        {
+
+         
+
+        }
+
+        private void onSwitchToggled(object sender, EventArgs e)
+
+        {
+
+    
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DateTime data = dpDate.Date;
+            DateTime data2 = dpDate1.Date;
+            Output.Text ="Wylot: "+data.ToString("yyyy/MM/dd")+" Przylot: "+data2.ToString("yyyy/MM/dd")+" Pasażerowie: "+Passengers.Text+" Miejsce Odlotu: "+Wylot.Text+" Miesjce Przylotu: "+Przylot.Text;
+        }
+    }
+}
